474e35ed7b9c4cfc4731123a0dd4b6a78953a5c1
ZidaneAKODA <mayoai556@gmail.com>
2025-05-18 11:51:10 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // service-worker.js
 
 // ⚠️ Incrémentez ce nom à chaque déploiement !
-const CACHE_NAME = 'bookshop-cache-v13';
+const CACHE_NAME = 'bookshop-cache-v14';
 
 const urlsToCache = [
   '/index.html',
